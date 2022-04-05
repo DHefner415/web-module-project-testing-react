a0d9af0250e227296079346caea81d6673b55e7c
DHefner415 <swurv415@gmail.com>
2022-04-05 16:04:26 -0700

    display tests completed

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,15 +1,55 @@
-import React from 'react';
-import '@testing-library/jest-dom/extend-expect';
-import { render, screen, waitFor} from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render, screen, waitFor } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
-import Display from './../Display';
+import Display from './../Display'
+import mockFetchShow from './../../api/fetchShow'
+jest.mock('./../../api/fetchShow')
 
+const testShow = {
+  name: 'test name',
+  summary: 'test summary',
+  seasons: [
+    {
+      id: 0,
+      name: 'Season 1',
+      episodes: [],
+    },
+    {
+      id: 1,
+      name: 'Season 2',
+      episodes: [],
+    },
+  ],
+}
 
-test('renders without errors with no props', ()=>{
+test('renders without errors with no props', () => {
+  render(<Display />)
+})
 
-});
+test('renders Show component when the button is clicked ', async () => {
+  mockFetchShow.mockResolvedValueOnce(testShow)
 
-test('renders Show component when the button is clicked ', ()=>{});
+  render(<Display />)
 
-test('renders show season options matching your data when the button is clicked', ()=>{});
+  const button = screen.getByRole('button')
+  userEvent.click(button)
+
+  const show = await screen.findByTestId('show-container')
+  expect(show).toBeInTheDocument()
+})
+
+test('renders show season options matching your data when the button is clicked', async () => {
+  mockFetchShow.mockResolvedValueOnce(testShow)
+
+  render(<Display />)
+
+  const button = screen.getByRole('button')
+  userEvent.click(button)
+
+  await waitFor(() => {
+    const seasonOption = screen.queryAllByTestId('season-option')
+    expect(seasonOption).toHaveLength(2)
+  })
+})

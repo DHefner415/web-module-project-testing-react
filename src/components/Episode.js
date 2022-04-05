2b6631c8bf333646291a1f190c0d62202664ee01
DHefner415 <swurv415@gmail.com>
2022-04-05 08:24:49 -0700

    show tests completed

--- src/components/Episode.js
+++ src/components/Episode.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+/*eslint-disable */
 const Episode = (props)=> {
     const {episode} = props;
     const {id, image, name, season, number, summary, runtime} = episode;
